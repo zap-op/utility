b09d3323e4895225cdd05aebca9f2e4b8f7c5191
Thanh Nguyen <thanhqng1510@gmail.com>
2022-11-29 20:39:44 +0700

    ZO-130 | Add type model to submodule

---
+++ model.ts
@@ -0,0 +1,32 @@
+import {ObjectId} from "mongoose";
+
+export type TTarget = {
+    userId: ObjectId,
+    name: string,
+    target: string,
+    tag?: [string]
+};
+
+export type TUser = {
+    sub: string,
+    email: string,
+    emailVerified: boolean,
+    name: string,
+    picture: string,
+    givenName: string,
+    familyName: string
+};
+
+export type TScanSession = {
+    url: string,
+    userId: ObjectId
+};
+
+export type TZapSpiderScanSession = {
+    scanConfig: {
+        maxChildren?: number,
+        recurse?: boolean,
+        contextName?: string,
+        subtreeOnly?: boolean
+    }
+};
