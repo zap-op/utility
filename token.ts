7c67d3f5ef0a21d11f6b94681656c8705e41a082
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-12-08 03:02:46 +0700

    ZO-148 | Update max age token to match milliseconds calculate.

--- token.ts
+++ token.ts
@@ -4,5 +4,5 @@ export enum TOKEN_TYPE {
     REFRESH = "refreshToken"
 }
 
-export const ACCESS_TOKEN_MAX_AGE = 3600; // 60 * 60
-export const REFRESH_TOKEN_MAX_AGE = 604800; // 7 * 24 * 60 * 60
+export const ACCESS_TOKEN_MAX_AGE = 3600000; // 60 * 60 * 1000
+export const REFRESH_TOKEN_MAX_AGE = 604800000; // 7 * 24 * 60 * 60 * 1000
