060b6459773c1a0ce642e86f80397acf2e4c60c1
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-12-10 02:27:32 +0700

    ZO-156 | Add user type and interface.

--- user.ts
+++ user.ts
@@ -1,3 +1,15 @@
+import { TOKEN_TYPE } from "./token";
+
+export interface JwtPayload {
+    iss?: string | undefined;
+    sub?: string | undefined;
+    aud?: string | string[] | undefined;
+    exp?: number | undefined;
+    nbf?: number | undefined;
+    iat?: number | undefined;
+    jti?: string | undefined;
+}
+
 export interface GgUserData {
     sub: string,
     email?: string,
@@ -7,3 +19,8 @@ export interface GgUserData {
     givenName?: string,
     familyName?: string
 }
+
+export type UserTokenData = JwtPayload & GgUserData & {
+    userId: string,
+    type: TOKEN_TYPE
+};
