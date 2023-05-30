d515a5b95ba1debac174effaee8687b1feb47410
Huy Pham <pham.giahuy.9041@gmail.com>
2023-05-30 11:22:05 +0700

    ZO-196 Update share scan type settings.

---
+++ settings.ts
@@ -0,0 +1,8 @@
+export enum ScanType {
+	NMAP_TCP = "nmap-tcp",
+	NMAP_UDP = "nmap-udp",
+	ZAP_SPIDER = "zap-spider",
+	ZAP_AJAX = "zap-ajax",
+	ZAP_ACTIVE = "zap-active",
+	ZAP_PASSIVE = "zap-passive",
+}
