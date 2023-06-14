b551078eb2f90559847a394e88172b486d9d3235
Thanh Nguyen <thanhqng1510@gmail.com>
2023-06-14 11:29:24 +0700

    temp

--- auth.ts
+++ auth.ts
@@ -1,33 +1,34 @@
 export enum TOKEN_TYPE {
-    GOOGLE = "ggToken",
-    ACCESS = "accessToken",
-    REFRESH = "refreshToken"
+	GOOGLE = "ggToken",
+	ACCESS = "accessToken",
+	REFRESH = "refreshToken",
 }
 
 export const ACCESS_TOKEN_MAX_AGE = 3600000; // 60 * 60 * 1000
 export const REFRESH_TOKEN_MAX_AGE = 604800000; // 7 * 24 * 60 * 60 * 1000
 
 export interface JwtPayload {
-    iss?: string | undefined;
-    sub?: string | undefined;
-    aud?: string | string[] | undefined;
-    exp?: number | undefined;
-    nbf?: number | undefined;
-    iat?: number | undefined;
-    jti?: string | undefined;
+	iss?: string | undefined;
+	sub?: string | undefined;
+	aud?: string | string[] | undefined;
+	exp?: number | undefined;
+	nbf?: number | undefined;
+	iat?: number | undefined;
+	jti?: string | undefined;
 }
 
 export interface GgUserData {
-    sub: string,
-    email?: string,
-    emailVerified?: boolean,
-    name?: string,
-    picture?: string,
-    givenName?: string,
-    familyName?: string
+	sub: string;
+	email?: string;
+	emailVerified?: boolean;
+	name?: string;
+	picture?: string;
+	givenName?: string;
+	familyName?: string;
 }
 
-export type UserTokenData = JwtPayload & GgUserData & {
-    userId: string,
-    type: TOKEN_TYPE
-};
+export type UserTokenData = JwtPayload &
+	GgUserData & {
+		userId: string;
+		type: TOKEN_TYPE;
+	};
--- status.ts
+++ status.ts
@@ -1,117 +1,117 @@
 const assert = <T>(el: T) => el;
 
 export enum ScanState {
-    PROCESSING = "PROCESSING",
-    SUCCESSFUL = "SUCCESSFUL",
-    FAILED = "FAILED"
-}
- 
-export type TStatusResponse = {
-    statusCode: number,
-    msg: string,
+	PROCESSING = "PROCESSING",
+	SUCCESSFUL = "SUCCESSFUL",
+	FAILED = "FAILED",
 }
 
+export type TStatusResponse = {
+	statusCode: number;
+	msg: string;
+};
+
 export const MGMT_STATUS = {
-    TARGET_ADDED: assert<TStatusResponse>({
-        statusCode: 10,
-        msg: "Target added successfully",
-    }),
-    TARGET_MOVED_TO_TRASH: assert<TStatusResponse>({
-        statusCode: 11,
-        msg: "Target moved to trash successfully",
-    }),
-    TARGET_DELETED: assert<TStatusResponse>({
-        statusCode: 12,
-        msg: "Target deleted successfully",
-    }),
-    TARGET_INVAVLID_URL: assert<TStatusResponse>({
-        statusCode: -11,
-        msg: "Invalid URL for target",
-    }),
-    TARGET_ADD_FAILED: assert<TStatusResponse>({
-        statusCode: -12,
-        msg: "Target failed to add",
-    }),
-    TARGET_INVALID_ID: assert<TStatusResponse>({
-        statusCode: -13,
-        msg: "Invalid ID for target",
-    }),
-    TARGET_FIND_FAILED: assert<TStatusResponse>({
-        statusCode: -14,
-        msg: "Target failed to find",
-    }),
-    TARGET_DELETE_FAILED: assert<TStatusResponse>({
-        statusCode: -15,
-        msg: "Target failed to delete",
-    }),
-    TARGET_NAME_DUPLICATE: assert<TStatusResponse>({
-        statusCode: -16,
-        msg: "Target name already exist",
-    }),
+	TARGET_ADDED: assert<TStatusResponse>({
+		statusCode: 10,
+		msg: "Target added successfully",
+	}),
+	TARGET_MOVED_TO_TRASH: assert<TStatusResponse>({
+		statusCode: 11,
+		msg: "Target moved to trash successfully",
+	}),
+	TARGET_DELETED: assert<TStatusResponse>({
+		statusCode: 12,
+		msg: "Target deleted successfully",
+	}),
+	TARGET_INVAVLID_URL: assert<TStatusResponse>({
+		statusCode: -11,
+		msg: "Invalid URL for target",
+	}),
+	TARGET_ADD_FAILED: assert<TStatusResponse>({
+		statusCode: -12,
+		msg: "Target failed to add",
+	}),
+	TARGET_INVALID_ID: assert<TStatusResponse>({
+		statusCode: -13,
+		msg: "Invalid ID for target",
+	}),
+	TARGET_FIND_FAILED: assert<TStatusResponse>({
+		statusCode: -14,
+		msg: "Target failed to find",
+	}),
+	TARGET_DELETE_FAILED: assert<TStatusResponse>({
+		statusCode: -15,
+		msg: "Target failed to delete",
+	}),
+	TARGET_NAME_DUPLICATE: assert<TStatusResponse>({
+		statusCode: -16,
+		msg: "Target name already exist",
+	}),
 };
 
 export const SCAN_STATUS = {
-    SESSION_INITIALIZE_SUCCEED: assert<TStatusResponse>({
-        statusCode: 90,
-        msg: "Scan session initialize succeed!",
-    }),
-    SESSION_INITIALIZE_FAIL: assert<TStatusResponse>({
-        statusCode: -91,
-        msg: "Scan session initialize fail!",
-    }),
-    INVAVLID_URL: assert<TStatusResponse>({
-        statusCode: -92,
-        msg: "Invalid URL!",
-    }),
-    INVALID_SESSION: assert<TStatusResponse>({
-        statusCode: -93,
-        msg: "Invalid scan session!",
-    }),
-    ZAP_INITIALIZE_FAIL: assert<TStatusResponse>({
-        statusCode: -94,
-        msg: "ZAP scan initialize fail!",
-    }),
-    ZAP_INTERNAL_ERROR: assert<TStatusResponse>({
-        statusCode: -95,
-        msg: "ZAP internal error!",
-    }),
-    INVALID_ID: assert<TStatusResponse>({
-        statusCode: -96,
-        msg: "Invalid scan id!",
-    }),
-    INVALID_RESULT_OFFSET: assert<TStatusResponse>({
-        statusCode: -97,
-        msg: "Invalid scan result offset!",
-    }),
-    INVALID_ID_OR_ZAP_INTERNAL_ERROR: assert<TStatusResponse>({
-        statusCode: -98,
-        msg: "Invalid scan id or ZAP internal error!",
-    }),
+	SESSION_INITIALIZE_SUCCEED: assert<TStatusResponse>({
+		statusCode: 90,
+		msg: "Scan session initialize succeed!",
+	}),
+	SESSION_INITIALIZE_FAIL: assert<TStatusResponse>({
+		statusCode: -91,
+		msg: "Scan session initialize fail!",
+	}),
+	INVAVLID_URL: assert<TStatusResponse>({
+		statusCode: -92,
+		msg: "Invalid URL!",
+	}),
+	INVALID_SESSION: assert<TStatusResponse>({
+		statusCode: -93,
+		msg: "Invalid scan session!",
+	}),
+	ZAP_INITIALIZE_FAIL: assert<TStatusResponse>({
+		statusCode: -94,
+		msg: "ZAP scan initialize fail!",
+	}),
+	ZAP_INTERNAL_ERROR: assert<TStatusResponse>({
+		statusCode: -95,
+		msg: "ZAP internal error!",
+	}),
+	INVALID_ID: assert<TStatusResponse>({
+		statusCode: -96,
+		msg: "Invalid scan id!",
+	}),
+	INVALID_RESULT_OFFSET: assert<TStatusResponse>({
+		statusCode: -97,
+		msg: "Invalid scan result offset!",
+	}),
+	INVALID_ID_OR_ZAP_INTERNAL_ERROR: assert<TStatusResponse>({
+		statusCode: -98,
+		msg: "Invalid scan id or ZAP internal error!",
+	}),
 };
 
 export const LOGIN_STATUS = {
-    LOGIN_SUCCESS: assert<TStatusResponse>({
-        statusCode: 0,
-        msg: "Login successfully"
-    }),
-    TOKEN_NOT_FOUND: assert<TStatusResponse>({
-        statusCode: -1,
-        msg: "No token found",
-    }),
-    TOKEN_INVALID: assert<TStatusResponse>({
-        statusCode: -2,
-        msg: "Invalid token",
-    }),
-    USER_ADD_FAILED: assert<TStatusResponse>({
-        statusCode: -3,
-        msg: "New user failed to add",
-    }),
-    USER_ALREADY_LINKED: assert<TStatusResponse>({
-        statusCode: -4,
-        msg: "Google account already used with different email",
-    }),
-    EMAIL_ALREADY_USED: assert<TStatusResponse>({
-        statusCode: -5,
-        msg: "Email already used",
-    }),
+	LOGIN_SUCCESS: assert<TStatusResponse>({
+		statusCode: 0,
+		msg: "Login successfully",
+	}),
+	TOKEN_NOT_FOUND: assert<TStatusResponse>({
+		statusCode: -1,
+		msg: "No token found",
+	}),
+	TOKEN_INVALID: assert<TStatusResponse>({
+		statusCode: -2,
+		msg: "Invalid token",
+	}),
+	USER_ADD_FAILED: assert<TStatusResponse>({
+		statusCode: -3,
+		msg: "New user failed to add",
+	}),
+	USER_ALREADY_LINKED: assert<TStatusResponse>({
+		statusCode: -4,
+		msg: "Google account already used with different email",
+	}),
+	EMAIL_ALREADY_USED: assert<TStatusResponse>({
+		statusCode: -5,
+		msg: "Email already used",
+	}),
 };
