6d2be7261570d5f54c6e0289de8f63bb22907994
thanh.nguyen <thanh.nguyen@opswat.com>
2023-06-21 17:56:51 +0700

    temp

--- api.ts
+++ api.ts
@@ -105,12 +105,6 @@ export type TZapSpiderResultsGETRequest = TZapSpiderTrialResultsGETRequest;
  */
 export type TZapSpiderResultsGETResponse = TZapSpiderTrialResultsGETResponse;
 
-export type TZapSpiderFullResultsParams = {
-	urlsInScopeOffset: number;
-	urlsOutOfScopeOffset: number;
-	urlsIoErrorOffset: number;
-};
-
 // /scan/zap/spider/fullResults
 /**
  * Request - GET - /scan/zap/spider/fullResults
